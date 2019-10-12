2bad12f9eb892ff5f8289b4e941467c913a7a484
wangqi <1553376706@qq.com>
2019-10-12 21:12:14 +0800

    初始化

---
+++ src/main/java/com/wangxile/redislock/config/RedisConfig.java
@@ -0,0 +1,43 @@
+package com.wangxile.redislock.config;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author:wangqi
+ * @Description:
+ * @Date:Created in 2019/10/12
+ * @Modified by:
+ */
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.pool.max-active}")
+    private int poolMaxActive;
+
+    @Value("${spring.redis.pool.max-idle}")
+    private int poolMaxIdle;
+
+    @Value("${spring.redis.pool.max-wait}")
+    private int poolMaxWait;
+
+}
